94fbc560afd7429e90db07da0f50961f1f1f654c
safiaOUAHAB <32792469+safiaOUAHAB@users.noreply.github.com>
2017-10-14 14:14:49 +0100

    Initial commit

---
+++ src/igl/traitement.java
@@ -0,0 +1,16 @@
+/
+package igl;
+
+
+public class traitement {
+     String chaines[];
+    
+     String  Remplir_chaines ( )
+               {int i ;
+                 for ( i=0; i < chaines.length;i++)
+                 {
+                     chaines[i] =s;
+                     
+                         }
+               }
+}
